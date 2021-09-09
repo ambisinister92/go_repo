86805cbd031857e1f23d3d2822a92ee0ae203eee
dmytro_fesenko <dmitryfesenko92@gmail.com>
2021-09-10 02:47:41 +0300

    formatting

--- hw-2/jsontoyaml/jsontoyaml.go
+++ hw-2/jsontoyaml/jsontoyaml.go
@@ -8,37 +8,35 @@ import (
 
 	"encoding/json"
 )
+
 type (
 	Sweets struct {
-		SweetsId string `json:"id" yaml:"id"`
-		SweetsType string `json:"type" yaml:"type"`
-		SweetsName     string    `json:"name" yaml:"name"`
-		Ppu float64 `json:"ppu" yaml:"ppu"`
-		Batters     Batters  `json:"batters" yaml:"batters"`
-		Topping []Topping `json:"topping" yaml:"topping"`
-
+		SweetsId   string    `json:"id" yaml:"id"`
+		SweetsType string    `json:"type" yaml:"type"`
+		SweetsName string    `json:"name" yaml:"name"`
+		Ppu        float64   `json:"ppu" yaml:"ppu"`
+		Batters    Batters   `json:"batters" yaml:"batters"`
+		Topping    []Topping `json:"topping" yaml:"topping"`
 	}
 
-	Batters struct{
+	Batters struct {
 		Batter []Batter `json:"batter" yaml:"batter"`
 	}
 
 	Batter struct {
-		BatterId string `json:"id" yaml:"id"`
-		BatterType      string `json:"type" yaml:"type"`
+		BatterId   string `json:"id" yaml:"id"`
+		BatterType string `json:"type" yaml:"type"`
 	}
 	Topping struct {
-		ToppingId string `json:"id" yaml:"id"`
+		ToppingId   string `json:"id" yaml:"id"`
 		ToppingType string `json:"type" yaml:"type"`
 	}
 )
 
+func GoJsonToYaml(s string) string {
 
-func GoJsonToYaml(s string) string{
-
-
-	jsonFile, err:=os.Open(s)
-	if err !=nil{
+	jsonFile, err := os.Open(s)
+	if err != nil {
 		log.Fatalf("failed to read: %v\n", err)
 	}
 	defer jsonFile.Close()
@@ -50,7 +48,6 @@ func GoJsonToYaml(s string) string{
 	if err := json.Unmarshal(byteValue, &sw1); err != nil {
 		log.Fatalf("failed to unmarshal: %v\n", err)
 	}
-	
 
 	enc, err := yaml.Marshal(sw1)
 	if err != nil {
--- hw-2/main.go
+++ hw-2/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"hw-2/jsontoyaml"
 	"hw-2/trafficlights"
 )
 
 func main() {
 
 	trafficlights.GoTrafficLights()
-	//fmt.Printf("%#v",goJsonToYaml.goJsonToYaml("lesson.json"),"\n")
+	fmt.Println(jsontoyaml.GoJsonToYaml("./lesson.json"))
 	fmt.Println("Done!")
 
 }
